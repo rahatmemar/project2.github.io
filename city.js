3a21f673d289fd13a32e66d0664ebc7470c23723
rahatmemar <153254567+rahatmemar@users.noreply.github.com>
2023-12-19 09:33:36 +0530

    Add files via upload

---
+++ city.js
@@ -0,0 +1,34 @@
+export const city =[
+    {
+        name :"Indiranagar",
+        place : 536,
+    },
+    {
+        name :"Marathahalli",
+        place : 919,
+    },
+    {
+        name :"Sarjapur Road",
+        place : 710,
+    },
+    {
+        name :"JP Nagar",
+        place : 650,
+    },
+    {
+        name :"Jayanagar",
+        place : 553,
+    },
+    {
+        name :"HSR",
+        place : 842,
+    },
+    {
+        name :"Koramangala",
+        place : 271,
+    },
+    {
+        name :"Whitefield",
+        place : 985,
+    }
+]
---
+++ index.js
@@ -0,0 +1,18 @@
+import { city } from "./city.js";
+
+
+let localitiesContainerEl = document.querySelector(".localities_container")
+let htmlLocalitiesList =city.map((el) =>{
+    return`
+    <div class="localities_list">
+        <div class="localities_card">
+            <h3>${el.name}</h3>
+                <p>${el.place}places</p>
+                 </div>
+                    <i class="fa-solid fa-angle-right"></i>
+     </div>
+    `
+    
+}).join("")
+
+localitiesContainerEl.innerHTML=htmlLocalitiesList
